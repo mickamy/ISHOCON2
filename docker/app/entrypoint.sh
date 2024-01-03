54d7586bb8b70ab58438e0bbc7e68caca4bd695a
mickamy <t.mikami1992@gmail.com>
2024-01-03 11:30:30 +0900

    db: fix database

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -11,9 +11,9 @@ sudo mysql -u root -pishocon -e "CREATE USER IF NOT EXISTS ishocon IDENTIFIED BY
 sudo mysql -u root -pishocon -e 'GRANT ALL ON *.* TO ishocon;' && \
 cd ~/data && tar -jxvf ishocon2.dump.tar.bz2 && sudo mysql -u root -pishocon ishocon2 < ~/data/ishocon2.dump
 sudo mysql -u root -pishocon -e '
+use ishocon2;
 ALTER TABLE votes ADD COLUMN count int(4) NOT NULL;
 ALTER TABLE votes ADD INDEX (candidate_id, count DESC);
-ALTER TABLE votes DROP COLUMN keyword;
 ALTER TABLE users ADD INDEX (name, address, mynumber);
 ';
 
